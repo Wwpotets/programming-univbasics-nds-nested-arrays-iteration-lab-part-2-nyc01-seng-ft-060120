af800851eee0b28dda5070a72be4fe25aae78080
Corey Lynch <bearinawolfpack@gmail.com>
2020-05-15 19:07:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,14 +2,14 @@ def find_min_in_nested_arrays(src)
   y_counter = 0
   	daily_lows = []
 
-  	while y_counter < array.length do
+  	while y_counter < src.length do
   		x_counter = 0
 
   		array_low = 100
 
-  		while x_counter < array[y_counter].length do
-  			if array[y_counter][x_counter] < array_low
-  				array_low = array[y_counter][x_counter]
+  		while x_counter < src[y_counter].length do
+  			if src[y_counter][x_counter] < array_low
+  				array_low = src[y_counter][x_counter]
   			end
 
         x_counter += 1
